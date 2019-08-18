dec93d8fb95eaff1b75d949c2e609e7bd481896a
Nic Leenknegt <nicolaas.leenknegt.w1123@student.hogent.be>
2019-08-19 00:27:25 +0200

    Added basis for new Category controller

---
+++ Repositories/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using Ghenterprise_Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ghenterprise_Backend.Repositories
+{
+
+    public class CategoryRepository
+    {
+        public string ConnString { get; set; }
+        public SSH Ssh { get; set; }
+        public KeyGenerator KeyGen { get; set; }
+
+        public CategoryRepository()
+        {
+            ConnString = "server=127.0.0.1;port=22;database=Ghenterprise;Uid=jari;Pwd=pazaak;";
+            Ssh = new SSH();
+            KeyGen = new KeyGenerator();
+        }
+
+
+    }
+}
